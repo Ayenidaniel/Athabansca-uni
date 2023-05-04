7bf276fed022c1de8428dceb9e5f4019078ee4cf
Ayenidaniel <ayenimayokun@gmail.com>
2023-05-04 07:20:22 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    fontFamily:{
+      'Tits' : ['Tilt Prism', 'cursive'],
+      'Roberto': ['Roboto Slab', 'serif'],
+      'Garamond': ['EB Garamond', 'serif'],
+      'Lobster': ['Lobster', 'cursive'],
+
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
